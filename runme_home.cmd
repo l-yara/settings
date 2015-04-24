94f3ce912ff63c1e165de9aa4fe35901af6c1473
Yaroslav Lyakh <Yaroslav.Lyakh@betfair.com>
2015-04-24 10:05:32 +0100

    added runme_home.cmd

---
+++ runme_home.cmd
@@ -0,0 +1,5 @@
+#to execute in <..>/.IntelliJIdea14/config:
+set SETTING_GIT_HOME=C:\Users\Lyakhy\settings\
+mklink .bashrc  %SETTING_GIT_HOME%\home\.bashrc
+mklink .profile  %SETTING_GIT_HOME%\home\.profile
+mklink .gitconfig  %SETTING_GIT_HOME%\home\.gitconfig
