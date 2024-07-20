099c98a352d86ca8ae8f207a40e13f77c69a5a18
l-yara <l_yara@bigmir.net>
2024-07-20 17:47:51 +0100

    Update runme_IDEA.sh

--- runme_IDEA.sh
+++ runme_IDEA.sh
@@ -1,10 +1,6 @@
-<<<<<<< Updated upstream
 #to execute in ~/Java/.IdeaIC15/config/ (/Users/yara/Library/Preferences/IdeaIC2019.3)
 export SETTING_GIT_HOME="/common/Google Drive/settings/IntellijIDEA/config"
-=======
-#to execute in ~/Java/.IdeaIC15/config/
-export SETTING_GIT_HOME="/Users/yara/settings/IntellijIDEA/config"
->>>>>>> Stashed changes
+#export SETTING_GIT_HOME="/Users/yara/settings/IntellijIDEA/config"
 #ln -s /common/Google\ Drive/settings/IntellijIDEA/config/codestyles/ codestyles
 
 ln -s "$SETTING_GIT_HOME/codestyles" codestyles
